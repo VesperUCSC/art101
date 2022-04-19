6c333241b48d4dc990ed36056480e867f289ecb0
VesperUCSC <iwaddy@ucsc.edu>
2022-04-18 18:48:00 -0700

    did lab 5

---
+++ lab5/js/lab.js
@@ -0,0 +1,21 @@
+/*
+ * Author: Vesper and Nailea
+ * Created: 18 April
+ * License: Public Domain
+ */
+
+ // Define Variables
+ make = "Crock";
+ model = "Pot";
+ color = "Black";
+ year = 1971;
+
+ //calculate
+ age = 2022 - year;
+
+ //output
+ document.writeln("Make: " + make + "<br>");
+ document.writeln("Model: " + model + "<br>");
+ document.writeln("Color: " + color + "<br>");
+ document.writeln("Year: " + year + "<br>");
+ document.writeln("Age: " + age + " years<br>");
