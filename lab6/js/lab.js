6b16da47fb10e845f1740262bb29a10bb9d2d113
VesperUCSC <iwaddy@ucsc.edu>
2022-04-27 11:19:32 -0700

    lab 6 edits

--- lab6/js/lab.js
+++ lab6/js/lab.js
@@ -5,19 +5,19 @@
  */
 
  //Define Variables
- myTransport = ["Car", "Bus"];
+ myTransport = ["Car", " Bus"];
 
  myMainRide = {
-   make: "Crock",
-   model: "Pot",
-   color: "black"
-   year: 1971
-   age: function() {
+   make : "Crock" ,
+   model : "Pot" ,
+   color : "black" ,
+   year : 1971 ,
+   age : function() {
       return 2022 - age;
    }
- }
+ };
 
  //Output
- document.writeIn("Kinds of transportation I use: ", myTransport, ",/br>");
+ document.writeln("Kinds of transportation I use: ", myTransport, "<br>");
  document.writeln("My Main Ride: <pre>",
-     JSON.stringify(myMainRide, null, '\t'), "</pre>");
+     JSON.stringify(myMainRide, null, '\t'), "<pre>");
